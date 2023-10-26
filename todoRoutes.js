9d0de5f3870340648d34b2c557e5ea5be0e960e0
IceScout26 <dhimasscout@gmail.com>
2023-10-26 15:04:19 +0700

    feat : router

---
+++ todoRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const db = require('./dbConfig'); // Impor konfigurasi database
+
+const router = express.Router();
+
+// Endpoint untuk mendapatkan daftar todo
+router.get('/todos', (req, res) => {
+    db.query('SELECT * FROM todo', (error, results) => {
+        if (error) {
+            throw error;
+        }
+        res.status(200).json(results.rows);
+    });
+});
+
+module.exports = router;
